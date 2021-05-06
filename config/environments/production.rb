63b8e98b7bad7a7097eb544bbe429df430bca326
JNFussion <juan.jose.gutierrez.99@gmail.com>
2021-05-06 19:49:52 +0200

    Fix typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,9 +63,9 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "odin_facebook_production"
 
   config.action_mailer.perform_caching = false
-  onfig.action_mailer.perform_deliveries = true
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  onfig.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
